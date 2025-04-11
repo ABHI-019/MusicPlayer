99591a47c3533c74b36b41d304f14cd13d78ae3d
Abhishek <abhishek.raj292002@gmail.com>
2025-04-12 00:01:47 +0530

    Add project files

---
+++ user service/dist/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import dotenv from "dotenv";
+import mongoose from 'mongoose';
+dotenv.config();
+const connectDb = async () => {
+    try {
+        mongoose.connect(process.env.MONGO_URL, {
+            dbName: "Spotify"
+        });
+    }
+    catch (error) {
+        console.log(error);
+    }
+};
+const app = express();
+app.get("/", (req, res) => {
+    res.send("server is running");
+});
+const port = process.env.PORT || 5000;
+app.listen(5000, () => {
+    console.log(`Server is running on port${port}`);
+});
