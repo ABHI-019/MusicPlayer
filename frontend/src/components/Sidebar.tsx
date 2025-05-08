560807b78ffd8b8861ae3745c4b6f7f6ad1fff46
Abhishek <abhishek.raj292002@gmail.com>
2025-05-08 13:08:03 +0530

    "contex create"

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -6,7 +6,7 @@ export const Sidebar = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="w-[25%] h-full flex-col gap-2 text-white hidden lg:flex">
+    <div className="w-[25%] h-full flex-col gap-2 mr-2 mt-2 ml-2 text-white hidden lg:flex">
       <div className="bg-[#121212] h-[15%] rounded flex flex-col justify-around">
         <div className="flex items-centre gap-3 pl-8 cursor-pointer"onClick={() => navigate("/")}>
           <img src="/home.png" alt="" className="w-6"/>
@@ -17,7 +17,7 @@ export const Sidebar = () => {
           <p className="font-bold">Search</p>
         </div>
       </div>
-      <div className="bg-[121212] h-[85%] rounded">
+      <div className="bg-[#121212] h-[85%] rounded">
         <div className="p-4 flex items-center justify-between">
           <div className="flex items-center gap-3">
             <img src="/stack.png" alt="" className="w-8"/>
@@ -32,7 +32,14 @@ export const Sidebar = () => {
           <PlayListCard/>
         </div>
 
-        <div className=""></div>
+        <div className="p-4 m-2 bg-[#121212] rounded font-semibold flex flex-col
+          items-start gap-1 pl-4 mt-4">
+          <h1>Let's FindSome  Podcast to follow </h1>
+          <p className="font-light"> We'll Keep you updated on new episodes </p>
+          <button className="px-4 py-1.5 bg-white text-black text-[15px] rounded-full mt-4">
+            Browse Podcasts
+          </button>
+        </div>
       </div>
     </div>
   )
