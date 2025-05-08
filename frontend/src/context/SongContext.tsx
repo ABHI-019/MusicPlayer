f167a22f368a4cc4d63300487f523176df24f369
Abhishek <abhishek.raj292002@gmail.com>
2025-05-08 23:37:07 +0530

    'sidebar'

---
+++ frontend/src/context/SongContext.tsx
@@ -0,0 +1,78 @@
+import axios from "axios";
+import React, {createContext,ReactNode,use,useCallback,useContext,useEffect,useState} from "react"
+
+
+const server ="http://localhost:8000"
+
+export interface Song {
+    id: string;
+    title: string;
+    description: string;
+    thumbnail: string;
+    audio: string;
+    album: string;
+}
+
+export interface Album {
+    id: string;
+    title: string;
+    description: string;
+    thumbnail: string;
+}
+
+interface SongContextType {
+    songs: Song[];
+    isPlaying: boolean;
+    setIsPlaying: (value: boolean) => void;
+    loading: boolean;
+    selectedSong: string | null;
+    setSelectedSong: (id: string | null) => void;
+}
+
+const SongContext = createContext<SongContextType |undefined >(undefined)
+
+interface SongProviderProps{
+    children: ReactNode
+}
+
+export const SongProvider: React.FC<SongProviderProps> = ({children}) => {
+    const [songs, setSongs] = useState<Song[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [selectedSong, setSelectedSong] = useState<string | null>(null);
+    const [isPlaying , setIsPlaying] = useState<boolean>(false)
+
+
+
+    const fetchSongs=useCallback (async() => {
+        setLoading(true)
+        try{
+            const {data} = await axios.get<Song[]>(`${server}/api/v1/song/all`)
+            setSongs(data)
+            if(data.length > 0){
+                setSelectedSong(data[0].id.toString())
+                setIsPlaying(false)
+            }
+        }catch(error){
+            console.error("Error fetching songs:", error)
+        }finally{
+            setLoading(false)
+        }
+        
+    },[]);
+    useEffect(()=>{
+        fetchSongs()
+    },[])
+    return (
+        <SongContext.Provider value={{songs,selectedSong, setselectedSong,loading,isPlaying,setIsPlaying}}>
+            {children} 
+        </SongContext.Provider>
+    )
+}
+
+export const useSongData = (): SongContextType => {
+    const context = useContext(SongContext)
+    if (!context) {
+        throw new Error("useSongData must be used within a SongProvider")
+    }
+    return context
+}
